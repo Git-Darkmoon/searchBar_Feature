63a2c30d288ce3e539abeca2f9a3688f08f3eeb6
Nicolas Mayorga <namayorgav@gmail.com>
2022-08-27 19:08:52 -0500

    Searchbar working

--- src/app.js
+++ src/app.js
@@ -1,31 +1,12 @@
-<<<<<<< HEAD
 const searchBar = document.getElementById("searchInput")
 const titles = document.querySelectorAll(".card-title")
 let userInput = ""
 
 searchBar.addEventListener("input", (e) => {
-  userInput = e.target.value
-  console.log(userInput)
+  userInput = e.target.value.toLowerCase()
 
-  for (i of titles) {
-    if (i.innerHTML.includes(userInput)) {
-      console.log(`${i.innerHTML} includes the input.`)
-    }
-  }
+  titles.forEach((card) => {
+    const isVisible = card.innerHTML.toLowerCase().includes(userInput)
+    card.parentElement.parentElement.classList.toggle("hide", !isVisible)
+  })
 })
-=======
-const searchBar = document.getElementById("searchInput")
-const titles = document.querySelectorAll(".card-title")
-let userInput = ""
-
-searchBar.addEventListener("input", (e) => {
-  userInput = e.target.value
-  console.log(userInput)
-
-  for (i of titles) {
-    if (i.innerHTML.includes(userInput)) {
-      console.log(`${i.innerHTML} includes the input.`)
-    }
-  }
-})
->>>>>>> 5f01b06de5360838b904df692256974654c7db11
